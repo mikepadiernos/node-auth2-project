85b07a4eecb33330e294edb0d3fc9307c73f880c
Michael Padiernos <mike.padiernos@gmail.com>
2020-04-25 16:29:31 -0400

    [BUILD] API / middleware, helper, secret
    - added restrict & generator middleware
    - cleaned up helper
    - coded the secret export

--- api/helpers/users-helper.js
+++ api/helpers/users-helper.js
@@ -2,13 +2,13 @@ const db = require('../../data/db-config.js');
 
 async function addUser(user) {
 	const [id] = await db("users")
-		.insert(user, "id");
+		.insert(user);
 	return findUserById(id);
 }
 
 function findUser() {
 	return db("users")
-		.select("id", "username");
+		.select("id", "username", "password");
 }
 
 function findUserBy(filter) {
--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,3 +1,7 @@
+
+const jwt = require('jsonwebtoken');
+const secrets = require('../secrets/users-secret.js');
+
 const logger = (req, res, next) => {
 	const method = req.method;
 	const endpoint = req.originalUrl;
@@ -7,13 +11,48 @@ const logger = (req, res, next) => {
 };
 
 const restrict = (req, res, next) => {
-	req.session && req.session.user
-		? next()
-		: res.status(401).json({ success: false, message: 'Content restricted. Please log in to access.' })
+
+	// req.session && req.session.user
+	// 	? next()
+	// 	: res.status(401).json({ success: false, message: 'Content restricted. Please log in to access.' })
+	console.log("Req: ", req.headers);
+	// console.log("Res: ", res);
+
+	try {
+		const token = req.headers.authorization;
+
+		if (token) {
+			jwt.verify(token, secrets.jwtSecret, (error, decodedToken) => {
+				if (error) {
+					res.status(401).json({ You: "Can't Touch This!" });
+				} else {
+					req.decodedJwt = decodedToken;
+					console.log(req.decodedJwt);
+					next();
+				}
+			})
+		} else {
+			throw new Error("Dark Helmet: Absolutely nothing! Which is what you are about to become.");
+		}
+	}
+	catch (error) {
+		res.status(401).json({ success: false, message: 'Content restricted. Please log in to access.' })
+	}
 }
 
-module
-	.exports = {
-		logger,
-		restrict,
+const generator = function genToken(user) {
+	const payload = {
+		userid: user.id,
+		username: user.username,
 	};
+	const options = { expiresIn: '1h' };
+	const token = jwt.sign(payload, secrets.jwtSecret, options);
+
+	return token;
+}
+
+module.exports = {
+	logger,
+	restrict,
+	generator
+};
